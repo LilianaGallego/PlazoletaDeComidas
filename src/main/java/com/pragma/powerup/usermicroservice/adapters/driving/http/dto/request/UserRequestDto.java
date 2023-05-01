80e83da8ab65d4e041ef5412852e2c22bb3ca854
LilianaGallego <lilianagallegom@gmail.com>
2023-04-30 20:00:55 -0500

    Validations for the user are added
    Se agregan las validaciones para cada uno de los campos requeridos del usuario DTO

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
@@ -1,11 +1,44 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @Getter
 public class UserRequestDto {
-    private Long idPerson;
+    @NotEmpty(message = "Name may not be empty")
+    @Size(min = 3, max = 40, message = "Name must be between 3 and 40 characters long")
+    private String name;
+
+    @NotEmpty(message = "Surname may not be empty")
+    @Size(min = 2, max = 40, message = "Surname must be between 2 and 40 characters long")
+    private String surname;
+
+    @NotEmpty(message = "Email may not be empty")
+    @Email(message = "The email is not valid")
+    private String mail;
+
+    @NotEmpty(message = "Phone may not be empty")
+    @Pattern(regexp = "^\\+?[0-9]{1,12}$", message = "The phone is not valid")
+    private String phone;
+
+    @NotEmpty(message = "Birthdate may not be empty")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date birthdate;
+
+    @NotEmpty(message = "DniNumber may not be empty")
+    @Pattern(regexp = "^[0-9]{1,10}$", message = "The dniNumber is not valid")
+    private String dniNumber;
+    @NotEmpty(message = "Password may not be empty")
+    private String password;
+    @NotEmpty(message = "IdRole may not be empty")
+    @Pattern(regexp = "^[0-9]{1,4}$", message = "The idRole is not valid")
     private Long idRole;
 }
