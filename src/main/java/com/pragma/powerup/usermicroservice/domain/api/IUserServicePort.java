64d224ade134a084781d4fea534614acccacb885
LilianaGallego <lilianagallegom@gmail.com>
2023-05-13 09:58:19 -0500

    refactor: "Imports are removed: because they are not being used"

--- src/main/java/com/pragma/powerup/usermicroservice/domain/api/IUserServicePort.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/api/IUserServicePort.java
@@ -1,9 +1,7 @@
 package com.pragma.powerup.usermicroservice.domain.api;
 
-import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.UserEntity;
 import com.pragma.powerup.usermicroservice.domain.model.User;
 
-import java.util.Optional;
 
 public interface IUserServicePort {
     void saveUserOwner(User user);
