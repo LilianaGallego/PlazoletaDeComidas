7a26ed6c37f4c850b50075f85a50204d1fa9fd1d
LilianaGallego <lilianagallegom@gmail.com>
2023-05-31 23:24:49 -0500

    test: "update test save User Employee"

--- src/test/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestControllerTest.java
+++ src/test/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestControllerTest.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.usermicroservice.adapters.driving.http.controller;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.UserRequestDto;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response.UserResponseDto;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.IUserHandler;
+import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.impl.UserHandlerImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -54,14 +55,21 @@ class UserRestControllerTest {
     @Test
     void testSaveUserEmployee() {
         // Arrange
-        Mockito.doNothing().when(userHandler).saveUserEmployee(userRequestDto);
+        UserRequestDto userRequestDto = new UserRequestDto();
+        // Set the necessary fields in the userRequestDto object
+
+        UserHandlerImpl userHandler = mock(UserHandlerImpl.class);
+        when(userHandler.saveUserEmployee(any(UserRequestDto.class))).thenReturn("123456789");
+
+        UserRestController userRestController = new UserRestController(userHandler);
 
         // Act
-        ResponseEntity<Map<String, String>> responseEntity = userRestController.saveUserEmployee(userRequestDto);
+        ResponseEntity<String> response = userRestController.saveUserEmployee(userRequestDto);
 
         // Assert
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals(Constants.USER_CREATED_MESSAGE, responseEntity.getBody().get(Constants.RESPONSE_MESSAGE_KEY));
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("123456789", response.getBody());
+        // Add additional assertions or verifications if needed
     }
 
     @Test
