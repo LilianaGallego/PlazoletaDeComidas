cc6f083d5eef461eef799ecdd647a1f3a3519f95
LilianaGallego <lilianagallegom@gmail.com>
2023-05-31 22:50:47 -0500

    build: "update endpoint create employee and implementation endpoint get employee by dniNumber"

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
@@ -72,21 +72,6 @@ public class UserRestController {
         return ResponseEntity.ok(userHandler.saveUserEmployee(userRequestDto));
     }
 
-    @Operation(summary = "Add a new client",
-            responses = {
-                    @ApiResponse(responseCode = "201", description = "Client created",
-                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
-                    @ApiResponse(responseCode = "409", description = "User already exists",
-                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
-                    @ApiResponse(responseCode = "403", description = "Role not allowed for user creation",
-                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
-    @PostMapping("/client/createClient")
-    public ResponseEntity<Map<String, String>> saveUserClient(@Valid @RequestBody UserRequestDto userRequestDto) {
-        userHandler.saveUserClient(userRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
-    }
-
     @Operation(summary = "Get a Employee user",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Employee user returned",
