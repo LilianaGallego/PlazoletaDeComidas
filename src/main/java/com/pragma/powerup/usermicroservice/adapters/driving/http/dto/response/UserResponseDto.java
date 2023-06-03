c7b6edb0fb6b90d06f28c64ca5efefa00bea23c0
LilianaGallego <lilianagallegom@gmail.com>
2023-06-03 12:12:25 -0500

    build: "Create employee is implemented and assigned to a restaurant"

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Getter
 public class UserResponseDto {
 
-    private String id;
+    private Long id;
     private String name;
     private String surname;
     private String mail;
